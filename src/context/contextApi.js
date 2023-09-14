b21b6c76b0ce97faf09e16fd40ac0a45602b07ce
Mohd Faiyaaz Khan <khanfaiyaaz179@gmail.com>
2023-09-14 14:53:29 +0530

    Header

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,41 +1,40 @@
 import React, { createContext, useState, useEffect } from "react";
 
 import { fetchDataFromApi } from "../utils/api";
-
 export const Context = createContext();
 
 export const AppContext = (props) => {
     const [loading, setLoading] = useState(false);
-    const [searchResults, setSearchResults] = useState(false);
-    const [selectCategory, setSelectCategory] = useState("New");
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
     const [mobileMenu, setMobileMenu] = useState(false);
 
     useEffect(() => {
-        fetchSelectedCategoryData(selectCategory);
-    }, [selectCategory]);
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
 
-    const fetchSelectedCategoryData=(query)=>{
+    const fetchSelectedCategoryData = (query) => {
         setLoading(true);
-        fetchDataFromApi(`search/?q=${query}`).then((res)=>{
-            console.log(res)
-            // setSearchResults(res)
-            setLoading(false)
-        })
-    }
+        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
 
     return (
-        <Context.Provider value={{
-            loading,
-            setLoading,
-            searchResults,
-            setSearchResults,
-            selectCategory,
-            setSelectCategory,
-            mobileMenu,
-            setMobileMenu
-        }}>
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
             {props.children}
         </Context.Provider>
-    )
-
-}
+    );
+};
